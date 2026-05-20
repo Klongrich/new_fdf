c80e3e5ce2bb30b035a4b30a11e8ec0cf80e3aba
klongrich <longrichk@gmail.com>
2026-05-20 14:27:23 -0400

    adding verification function

--- src/main.c
+++ src/main.c
@@ -1,10 +1,50 @@
 #include "fdf.h"
 
-void	read_file(char *file_path)
+int		contains_number(char *str) {
+	int i;
+
+	i = 0;
+	while (str[i]) {
+		if (!ft_isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int		get_rowlen(char	*row) {
+	char **temp;
+	int i;
+
+	i = 0;
+	temp = ft_strsplit(row, ' ');
+	while (temp[i]) {
+		if (!contains_number(temp[i]))
+			return (-1);
+		i++;
+	}
+	free(temp);
+	return (i);
+}
+
+int	print_error(int e) {
+	if (e == -1)
+		ft_printf("file does not contain all numbers\n");
+	else
+		ft_printf("Row sizes are not the same\n");
+	return (0);
+}
+
+
+int		verify_file(char *file_path)
 {
 	int fd;
 	char *str;
+	int	row_len;
+	int i;
 
+	i = 0;
+	row_len = 0;
 	fd = open(file_path, O_RDONLY, S_IRUSR);
 	if (fd == -1)
 		ft_printf("error opening file\n");
@@ -12,9 +52,15 @@ void	read_file(char *file_path)
 	{
 		while(get_next_line(fd, &str))
 		{
-			ft_printf("str: %s\n", str); 
+			if (i == 0)
+				row_len = get_rowlen(str);
+			if (row_len != get_rowlen(str))
+				return(print_error(get_rowlen(str)));
+			ft_printf("str: %s\n", str);
+			i++;
 		}
 	}
+	return (1);
 }
 
 int main(int argc, char **argv) 
@@ -31,8 +77,7 @@ int main(int argc, char **argv)
 	}
 	else
 	{
-		read_file(argv[1]);
-		ft_printf("valid formatting\n");		
+		verify_file(argv[1]);
 	}
 
 	return (0);
