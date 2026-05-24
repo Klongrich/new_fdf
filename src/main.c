f560c119d73c26b4b1fc335e6ee0ebba92f32d15
klongrich <longrichk@gmail.com>
2026-05-24 09:04:19 -0400

    adding ability to hold down right arrow key and move image

--- src/main.c
+++ src/main.c
@@ -382,6 +382,8 @@ void my_keyhook(mlx_key_data_t keydata, void* param)
 
 	if (keydata.key == MLX_KEY_RIGHT && keydata.action == MLX_PRESS)
 		data->img->instances[0].x += 5;
+	if (keydata.key == MLX_KEY_RIGHT && keydata.action == MLX_REPEAT)
+		data->img->instances[0].x += 5;
 	if (keydata.key == MLX_KEY_LEFT && keydata.action == MLX_PRESS)
 		data->img->instances[0].x -= 5;
 	if (keydata.key == MLX_KEY_UP && keydata.action == MLX_PRESS)
