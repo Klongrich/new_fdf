e1f5b5027d22b9d891f68f152d8d92e0f1b93566
klongrich <longrichk@gmail.com>
2026-05-25 10:58:18 -0400

    adding hold down for key down

--- src/main.c
+++ src/main.c
@@ -396,6 +396,9 @@ void my_keyhook(mlx_key_data_t keydata, void* param)
 
 	if (keydata.key == MLX_KEY_DOWN && keydata.action == MLX_PRESS)
 		data->img->instances[0].y += 5;
+	 if (keydata.key == MLX_KEY_DOWN && keydata.action == MLX_PRESS)
+		data->img->instances[0].y += 5;
+
 	if (keydata.key == MLX_KEY_MINUS && keydata.action == MLX_PRESS)
 	{
 		ft_memset(data->img->pixels, 0, data->img->width * data->img->height * 4);
