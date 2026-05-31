fba5fe753cb224e79d967872e445f996dea4a7b3
klongrich <longrichk@gmail.com>
2026-05-31 09:45:21 -0400

    important update, do not revert even if the build is broken

--- src/handle_keys.c
+++ src/handle_keys.c
@@ -30,6 +30,7 @@ void my_keyhook(mlx_key_data_t keydata, void* param)
 	if (keydata.key == MLX_KEY_M && keydata.action == MLX_PRESS)
 	{
 		data->rotation += 1;
+
 		apply_rotation(data, data->rotation);
 	}
 }
