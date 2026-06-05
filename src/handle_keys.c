ac47e7b41d4b1ae348fee8cca68391cce4bd4a7d
klongrich <longrichk@gmail.com>
2026-06-05 13:22:43 -0400

    adding counter clockwise rotation

--- src/handle_keys.c
+++ src/handle_keys.c
@@ -31,7 +31,11 @@ void my_keyhook(mlx_key_data_t keydata, void* param)
 	{
 		data->rotation += 1;
 		apply_rotation(data, data->rotation);
-	
-
 	}
+	if (keydata.key == MLX_KEY_N && keydata.action == MLX_PRESS)
+	{
+		data->rotation -=1;
+		apply_rotation(data, data->rotation);
+	}
+
 }
